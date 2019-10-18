2dda722a37eca99006c48a90b9a5901d34b02885
William Johnsson <wjohnsson@gmail.com>
2019-10-18 13:53:17 +0200

    Dont look at next track piece
    The result is the same if we check the track piece after moving

--- 2018/13/minecart_madness.py
+++ 2018/13/minecart_madness.py
@@ -4,18 +4,16 @@ from cart import Cart
 from cart import cart_symbols
 
 dirname = os.path.dirname(__file__)
-filename = os.path.join(dirname, "input_track")
+file_path = os.path.join(dirname, "test_small_track")
 
-tracks = [x.rstrip() for x in open(filename).readlines()]
+tracks = [x.rstrip() for x in open(file_path).readlines()]
 carts = []
 
-cart_id = 0
 for y in range(len(tracks)):
     for x in range(len(tracks[y])):
         symbol = tracks[y][x]
         if symbol in cart_symbols:
             carts.append(Cart(x, y, symbol))
-            cart_id += 1
 
             # Place a track piece underneath this cart
             if symbol in "v^":
@@ -24,39 +22,21 @@ for y in range(len(tracks)):
                 tracks[y] = tracks[y].replace(symbol, "-", 1)
 
 
-def next_track_piece():
-    """Determines the next piece of track a cart will be on"""
-
-    try:
-        if cart.dir == "^":
-            next_piece = tracks[cart.y - 1][cart.x]
-        elif cart.dir == "v":
-            next_piece = tracks[cart.y + 1][cart.x]
-        elif cart.dir == "<":
-            next_piece = tracks[cart.y][cart.x - 1]
-        elif cart.dir == ">":
-            next_piece = tracks[cart.y][cart.x + 1]
-        return next_piece
-    except IndexError:
-        print("cart: " + cart.verbose_repr())
-        print("tick: " + str(tick))
-        raise
-        exit()
-
-
 def turn_cart():
     """Turns a cart if it is now standing on a curve or intersection"""
-    if next_track == "\\":
+    track = tracks[cart.y][cart.x]
+
+    if track == "\\":
         if cart.dir in "^v":
             cart.left()   # ⮡ ⮢
         elif cart.dir in "<>":
             cart.right()  # ⮧ ⮤
-    elif next_track == "/":
+    elif track == "/":
         if cart.dir in "^v":
             cart.right()  # ⮣ ⮠
         elif cart.dir in "<>":
             cart.left()   # ⮥ ⮦
-    elif next_track == "+":
+    elif track == "+":
         cart.turn()
 
 
@@ -90,7 +70,6 @@ while True:
     list.sort(carts)  # top most cart moves first
     positions = set()
     for cart in carts:
-        next_track = next_track_piece()
         cart.move()
 
         len_before = len(positions)
