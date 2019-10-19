171b3ca8b3ddd7a5653752f0a16160ec2782da3b
William Johnsson <wjohnsson@gmail.com>
2019-10-19 14:05:32 +0200

    Some small refactoring

--- 2018/13/minecart_madness.py
+++ 2018/13/minecart_madness.py
@@ -4,6 +4,10 @@ from cart import cart_symbols
 
 
 def create_map(input_file_lines):
+    """
+    Sets up tracks and carts by reading input file in order to start
+    simulating
+    """
     tracks = [l.rstrip() for l in input_file_lines]
     carts = []
     for y in range(len(tracks)):
@@ -22,24 +26,26 @@ def create_map(input_file_lines):
 
 def turn_cart(tracks, cart):
     """Turns a cart if it is now standing on a curve or intersection"""
-    tracks = tracks[cart.y][cart.x]
+    track = tracks[cart.y][cart.x]
 
-    if tracks == "\\":
+    if track == "\\":
         if cart.dir in "^v":
             cart.left()   # ⮡ ⮢
         elif cart.dir in "<>":
             cart.right()  # ⮧ ⮤
-    elif tracks == "/":
+
+    if track == "/":
         if cart.dir in "^v":
             cart.right()  # ⮣ ⮠
         elif cart.dir in "<>":
             cart.left()   # ⮥ ⮦
-    elif tracks == "+":
+
+    if track == "+":
         cart.turn()
 
 
 def first_crash(input_file_lines):
-    """Prints solution to part one, the location of the first crash"""
+    """ Returns solution to part one, the location of the first crash """
     tracks, carts = create_map(input_file_lines)
 
     tick = 0  # used for debugging
@@ -60,8 +66,8 @@ def first_crash(input_file_lines):
         tick += 1
 
 
-# part one
-# dirname = os.path.dirname(__file__)
-# file_path = os.path.join(dirname, "test_small_track")
-# print("Crash at " +
-#       str(first_crash(open(file_path).readlines())))
+def answer_part_one():
+    dirname = os.path.dirname(__file__)
+    file_path = os.path.join(dirname, "input_track")
+    print("Crash at " +
+        str(first_crash(open(file_path).readlines())))
