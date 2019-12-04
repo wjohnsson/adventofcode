f7795c9094582aa64b9f7630508275ab1a674c8f
William Johnsson <wjohnsson@gmail.com>
2019-12-04 12:40:24 +0100

    19/04: Extract ascending check to separate function

--- 2019/04/secure_container.py
+++ 2019/04/secure_container.py
@@ -4,35 +4,36 @@ lower = 356261
 upper = 846303
 
 
+def is_ascending(num):
+    while num:
+        prev = num % 10
+        num //= 10
+        curr = num % 10
+
+        if prev < curr:
+            # Digits are decreasing going from left to right
+            return False
+    return True
+
+
 def first_criteria(n):
-    double_digits = False
     while n:
         prev = n % 10
         n //= 10
         curr = n % 10
 
-        if prev < curr:
-            # Digits are decreasing going from left to right
-            return False
         if prev == curr:
             # Two adjacent digits are the same
-            double_digits = True
-    return double_digits
+            return True
+    return False
 
 
 def second_criteria(n):
     s = str(n)
-    while n:
-        prev = n % 10
-        n //= 10
-        curr = n % 10
-        if prev < curr:
-            # Digits are decreasing going from left to right
-            return False
-
     # If we have a group of length 2 we know there's two adjacent digits
     return any([len(list(g)) == 2 for _, g in groupby(s)])
 
 
-print(len([n for n in range(lower, upper + 1) if first_criteria(n)]))  # 544
-print(len([n for n in range(lower, upper + 1) if second_criteria(n)]))  # 334
+print()
+print(len([n for n in range(lower, upper + 1) if is_ascending(n) and first_criteria(n)]))  # 544
+print(len([n for n in range(lower, upper + 1) if is_ascending(n) and second_criteria(n)]))  # 334
