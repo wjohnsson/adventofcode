12e808f19acf9ac5967950e67a5977129c50f956
William Johnsson <wjohnsson@gmail.com>
2019-12-05 22:15:13 +0100

    19/05: Welcome to If City, where you might enjoy your stay!
    Part 2 complete

--- 2019/05/termal_est.py
+++ 2019/05/termal_est.py
@@ -1,19 +1,17 @@
-import operator
-
-# Opcodes
-ADD = 1
-MUL = 2
-IN = 3
-OUT = 4
-HALT = 99
+# Instruction set
+ADD = 1    # Addition
+MUL = 2    # Multiplication
+IN = 3     # Get user input
+OUT = 4    # Print
+JNZ = 5    # Jump if Non-Zero
+JEZ = 6    # Jump if Zero
+SLT = 7    # Store 1 if Less Than, 0 otherwise
+EQ = 8     # Store 1 if equal, 0 otherwise
+HALT = 99  # Halt program
 
 # Parameter modes
 POS = 0  # positional
 IMM = 1  # immediate
-
-
-input_line = open("input.txt").readline().strip()
-intcode = [int(n) for n in input_line.split(",")]
 
 
 def parse_instruction(instr):
@@ -22,42 +20,84 @@ def parse_instruction(instr):
     return opcode, param_modes
 
 
-def arith_op(mem, pc, op, param_modes):
-    """Return result of applying op to the instruction parameters"""
-    if param_modes.pop() == IMM:
-        a = mem[pc + 1]
-    else:
-        a = mem[mem[pc + 1]]
-    if param_modes.pop() == IMM:
-        b = mem[pc + 2]
-    else:
-        b = mem[mem[pc + 2]]
-    return op(a, b)
+def get_params(mem, pc, param_modes):
+    """Return two parameters using parameter modes."""
+    params = []
+    for i in range(2):
+        mode = param_modes.pop()
+        if mode == IMM:
+            params.append(mem[pc + 1 + i])
+        elif mode == POS:
+            params.append(mem[mem[pc + 1 + i]])
+        else:
+            raise ValueError
+    return params[0], params[1]
 
 
-def run_intcode(mem):
-    """Run the intcode on the ship computer"""
+def run_intcode(mem, initial_input):
+    """Run the intcode on the ship computer."""
     pc = 0  # program counter
     while True:
         opcode, param_modes = parse_instruction(mem[pc])
 
+        # Welcome to If City, enjoy your stay.
         if opcode == HALT:
             break
         elif opcode == IN:
-            mem[mem[pc + 1]] = 1  # we always input 1 anyways
+            mem[mem[pc + 1]] = initial_input
             pc += 2
         elif opcode == OUT:
             print(mem[mem[pc + 1]])
             pc += 2
-        elif opcode == ADD:
-            mem[mem[pc + 3]] = arith_op(mem, pc, operator.add, param_modes)
-            pc += 4
-        elif opcode == MUL:
-            mem[mem[pc + 3]] = arith_op(mem, pc, operator.mul, param_modes)
-            pc += 4
         else:
-            print("unknown opcode " + str(opcode) + " exiting...")
-            break
+            # All other operations have at least 2 params
+            p1, p2 = get_params(mem, pc, param_modes)
+            p3 = mem[pc + 3]  # third parameter, always positional
+
+            if opcode == ADD:
+                mem[p3] = p1 + p2
+                pc += 4
+
+            elif opcode == MUL:
+                mem[p3] = p1 * p2
+                pc += 4
+
+            elif opcode == JNZ:
+                if p1 != 0:
+                    pc = p2
+                else:
+                    pc += 3
+
+            elif opcode == JEZ:
+                if p1 == 0:
+                    pc = p2
+                else:
+                    pc += 3
+
+            elif opcode == SLT:
+                if p1 < p2:
+                    mem[p3] = 1
+                else:
+                    mem[p3] = 0
+                pc += 4
+
+            elif opcode == EQ:
+                if p1 == p2:
+                    mem[p3] = 1
+                else:
+                    mem[p3] = 0
+                pc += 4
+
+            else:
+                print("unknown opcode " + str(opcode) + " exiting...")
+                break
 
 
-run_intcode(intcode)  # 9654885
+input_line = open("input.txt").readline().strip()
+intcode = [int(n) for n in input_line.split(",")]
+
+# Part 1
+run_intcode(intcode[:], 1)  # 9654885
+
+# Part 2
+run_intcode(intcode[:], 5)  # 7079459
