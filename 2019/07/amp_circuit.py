b7b55a0ba32186fd2ccf653330709e2642cb8614
William <wjohnsson@gmail.com>
2019-12-19 14:44:16 +0100

    19/07: Amps are amped
    Clean up code for part 1 and complete part 2.

--- 2019/07/amp_circuit.py
+++ 2019/07/amp_circuit.py
@@ -1,24 +1,46 @@
 from intcode_vm import IntcodeVM
 from itertools import permutations
 
+
+def run_serial_amps(code, phases):
+    amps = init_amps(code, phases)
+    signal = 0
+    for amp in amps:
+        amp.next_input(signal)
+        signal = amp.run()
+    return signal
+
+
+def run_feedback_amps(code, phases):
+    amps = init_amps(code, phases)
+    signal = 0
+    halt = False
+    while not halt:
+        for amp in amps:
+            amp.next_input(signal)
+            try:
+                signal = amp.run()
+            except StopIteration:
+                halt = True
+    return signal
+
+
+def init_amps(code, phases):
+    amps = [IntcodeVM(code[:])]  # last amp
+    for i in range(4):
+        amps.append(IntcodeVM(intcode[:]))
+    amps.reverse()
+
+    for i, phase in enumerate(phases):
+        amps[i].next_input(phase)
+    return amps
+
+
 input_line = open("input.txt").readline()
 intcode = [int(n) for n in input_line.split(",")]
 
-
-def run_vms(phases):
-    vms = [IntcodeVM(intcode[:], None)]  # last amp
-    for i in range(4):
-        vms.append(IntcodeVM(intcode[:], vms[i]))
-    vms.reverse()
-
-    for i, phase in enumerate(phases):
-        vms[i].next_input(phase)
-    vms[0].next_input(0)
-
-    for vm in vms[:-1]:
-        vm.run()
-    return vms[4].run()
-
-
 # Part 1
-print(max([run_vms(phases) for phases in permutations(range(0, 5))]))
+print(max([run_serial_amps(intcode, phases) for phases in permutations(range(0, 5))]))
+
+# Part 2
+print(max([run_feedback_amps(intcode, phases) for phases in permutations(range(5, 10))]))
--- 2019/07/intcode_vm.py
+++ 2019/07/intcode_vm.py
@@ -15,15 +15,14 @@ HALT = 99  # Halt program
 POS = 0  # positional
 IMM = 1  # immediate
 
-ARITY = {ADD: 3, MUL: 3, IN: 1, OUT: 1, JNZ: 2, JEZ: 2, SLT: 3, EQ: 3}
+ARITY = {ADD: 3, MUL: 3, IN: 1, OUT: 1, JNZ: 2, JEZ: 2, SLT: 3, EQ: 3, 99: 0}
 
 
 class IntcodeVM:
-    def __init__(self, mem, output_vm):
+    def __init__(self, mem):
         self.mem = mem
         self.pc = 0
         self.input_queue = SimpleQueue()
-        self.output_vm = output_vm
 
     @staticmethod
     def parse_instruction(instr):
@@ -49,62 +48,59 @@ class IntcodeVM:
 
     def run(self):
         """
-        Run VM until halt or input queue is empty
+        Run VM until halt or output is produced or input queue is empty
         """
         mem = self.mem
-        pc = self.pc
         while True:
-            opcode, param_modes = self.parse_instruction(mem[pc])
+            opcode, param_modes = self.parse_instruction(mem[self.pc])
 
             if opcode == HALT:
-                break
+                raise StopIteration
 
-            params = self.get_params(mem, pc, param_modes, ARITY[opcode])
-            p3 = mem[pc + 3]  # third parameter, always positional
+            params = self.get_params(mem, self.pc, param_modes, ARITY[opcode])
+            if ARITY[opcode] > 2:
+                p3 = mem[self.pc + 3]  # third parameter, always positional
 
             if opcode == IN:
-                mem[mem[pc + 1]] = self.input_queue.get()  # blocks if empty
-                pc += 2
+                mem[mem[self.pc + 1]] = self.input_queue.get()  # blocks if empty
+                self.pc += 2
             elif opcode == OUT:
-                if self.output_vm is None:
-                    return params[0]
-                else:
-                    self.output_vm.next_input(params[0])
-                pc += 2
+                self.pc += 2
+                return params[0]
 
             elif opcode == ADD:
                 mem[p3] = params[0] + params[1]
-                pc += 4
+                self.pc += 4
 
             elif opcode == MUL:
                 mem[p3] = params[0] * params[1]
-                pc += 4
+                self.pc += 4
 
             elif opcode == JNZ:
                 if params[0] != 0:
-                    pc = params[1]
+                    self.pc = params[1]
                 else:
-                    pc += 3
+                    self.pc += 3
 
             elif opcode == JEZ:
                 if params[0] == 0:
-                    pc = params[1]
+                    self.pc = params[1]
                 else:
-                    pc += 3
+                    self.pc += 3
 
             elif opcode == SLT:
                 if params[0] < params[1]:
                     mem[p3] = 1
                 else:
                     mem[p3] = 0
-                pc += 4
+                self.pc += 4
 
             elif opcode == EQ:
                 if params[0] == params[1]:
                     mem[p3] = 1
                 else:
                     mem[p3] = 0
-                pc += 4
+                self.pc += 4
 
             else:
                 print("unknown opcode " + str(opcode) + " exiting...")
