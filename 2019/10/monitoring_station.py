75ba3db68c86a88c72ee295366a0d0844c89ecbc
William Johnsson <wjohnsson@gmail.com>
2019-12-24 00:37:00 +0100

    19/10: imma firin mah lazer

--- 2019/10/monitoring_station.py
+++ 2019/10/monitoring_station.py
@@ -1,17 +1,17 @@
-from math import gcd as greatest_common_divisor
+import math
 
 
-def visible_count(station, asteroids):
-    unique = set()  # set of unique directions
+def visible_asteroids(station, asteroids):
+    visible = set()  # set of visible asteroids
     for asteroid in asteroids:
         if asteroid == station:
             continue
         dx = asteroid[0] - station[0]
         dy = asteroid[1] - station[1]
-        gcd = greatest_common_divisor(dx, dy)
-        unique.add((dx // gcd, dy // gcd))
+        gcd = math.gcd(dx, dy)
+        visible.add((dx // gcd, dy // gcd))
 
-    return len(unique)
+    return station, visible
 
 
 def asteroid_positions(inp):
@@ -19,7 +19,30 @@ def asteroid_positions(inp):
             if inp[y][x] == "#"]
 
 
+def fire_lazer(station, visible, asteroids):
+    # Since the 200th asteroid is visible, we only need to do one rotation
+    # Flip atan2 arguments to start angles from y-axis
+    vaporized = [(math.atan2(dx, dy), (dx, dy)) for dx, dy in visible]
+
+    # Use reverse sort so that smallest angles (negative) are vaporized last
+    vaporized.sort(reverse=True)
+    dx, dy = vaporized[200-1][1]
+
+    # Find actual coordinate of 200th vaporized asteroid
+    x, y = station[0]+dx, station[1]+dy
+    while (x, y) not in asteroids:
+        x, y = x+dx, y+dy
+
+    return x*100 + y
+
+
 input_lines = open("input.txt").read().splitlines()
 asteroids = asteroid_positions(input_lines)
+options = [visible_asteroids(a, asteroids) for a in asteroids]
+best_station, visible = max(options, key=lambda x: len(x[1]))
+
 # Part 1
-print(max([visible_count(a, asteroids) for a in asteroids]))  # 288
+print(len(visible))  # 288
+
+# Part 2
+print(fire_lazer(best_station, visible, asteroids))  # 616
