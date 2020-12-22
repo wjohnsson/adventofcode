6a244d310b70d2be2cd6c86d0dd0e29f4bbd7a42
William Johnsson <wjohnsson@gmail.com>
2020-12-22 11:57:37 +0100

    20/17: Conway cubes in arbitrary dimensions (4d for part 2)
    Clean solution today :-)

--- 2020/17/conway_cubes.py
+++ 2020/17/conway_cubes.py
@@ -1,15 +1,15 @@
-from itertools import permutations
+from itertools import product
 from collections import defaultdict
 
 # State
 active, active_neighbors = set(), defaultdict(int)
 
 # When applied - yields all neighbors of a given coordinate
-deltas = set(permutations([0, 0, 1, 1, 1, -1, -1, -1], 3))
+deltas = set()
 
 
 def main():
-    global active
+    global active, active_neighbors, deltas
     # Input
     starting_state = ("..##.#.#"
                       "\n##....#."
@@ -20,33 +20,59 @@ def main():
                       "\n##...##."
                       "\n.#...#..")
 
-    # starting_state = ".#.\n..#\n###"  # Test
-    parse_input(starting_state)
+    # Test
+    # starting_state = ".#.\n..#\n###"
 
-    for _ in range(6):
+    # ---- Part 1 ----
+    # 3D
+    deltas = get_deltas(3)
+    parse_input(starting_state, 3)
+
+    simulate(6)
+    print(f"Part 1: {len(active)}")  # 202
+
+    # ---- Part 2 ----
+    # 4D
+    active, active_neighbors = set(), defaultdict(int)  # reset state
+    deltas = get_deltas(4)
+    parse_input(starting_state, 4)
+
+    simulate(6)
+    print(f"Part 2: {len(active)}")  # 2028
+
+
+def parse_input(starting_state, dimensions):
+    global active
+    # Set top left starting state at coordinate system origin
+    pos = [0] * dimensions
+    for line in starting_state.splitlines():
+        pos[0] = 0
+        for char in line:
+            if char == "#":
+                active.add(tuple(pos))
+                add_neighbors(1, tuple(pos))
+            pos[0] += 1
+        pos[1] -= 1
+
+
+def get_deltas(dimensions):
+    ds = set(product([0, 1, -1], repeat=dimensions))
+    ds.remove(tuple([0] * dimensions))
+    assert len(ds) == 3 ** dimensions - 1  # https://oeis.org/A024023
+    return ds
+
+
+def simulate(steps):
+    global active
+
+    for _ in range(steps):
         activate, deactivate = one_cycle()
         for pos in activate:
             active.add(pos)
-            add_neighbors(1, *pos)
+            add_neighbors(1, pos)
         for pos in deactivate:
             active.remove(pos)
-            add_neighbors(-1, *pos)
-
-    print(len(active))
-
-
-def parse_input(starting_state):
-    global active
-    # Set top left starting state at coordinate system origin
-    z = y = 0
-    for line in starting_state.splitlines():
-        x = 0
-        for char in line:
-            if char == "#":
-                active.add((x, y, z))
-                add_neighbors(1, x, y, z)
-            x += 1
-        y -= 1
+            add_neighbors(-1, pos)
 
 
 def one_cycle():
@@ -67,10 +93,12 @@ def one_cycle():
     return activate, deactivate
 
 
-def add_neighbors(amount, x, y, z):
+def add_neighbors(amount, pos):
     global deltas, active_neighbors
-    for dx, dy, dz in deltas:
-        active_neighbors[x + dx, y + dy, z + dz] += amount
+    for delta in deltas:
+        # neighbor_pos = [pos.x + dx, pos.y + dy, ...]
+        neighbor_pos = tuple([pos[i] + delta[i] for i in range(len(pos))])
+        active_neighbors[neighbor_pos] += amount
 
 
 if __name__ == "__main__":
